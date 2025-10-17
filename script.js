45dc1469af4a92666164156ee6575a5e338f12f3
Rafael Ribeiro <raphahribs@icloud.com>
2025-10-17 23:21:13 +0100

    removing script.js file to avoid redundancy

--- script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    document.querySelectorAll('.produto').forEach(function(produto) {
-      if (produto.getBoundingClientRect().top < window.innerHeight) {
-        produto.classList.add('is-visible');
-      }
-    });
-  });
-  
